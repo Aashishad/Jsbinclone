b5f36f5a3ab0390c0b45d4986fbb31d2ab3d1bbc
Aashishad <aashishrestha15@gmail.com>
2024-04-25 12:29:34 +0545

    changes

---
+++ src/components/Editor.jsx
@@ -0,0 +1,25 @@
+import { Box } from '@mui/material'
+import React from 'react'
+
+const Editor = () => {
+  return (
+    <Box>
+      <Box>
+        <Box>
+          <Box component = "span">
+            
+          </Box>
+        
+        </Box>
+      </Box>
+      <Box>
+        
+      </Box>
+
+    </Box>
+  
+    
+  )
+}
+
+export default Editor
